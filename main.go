0e5f3b6328b9ae3bb21c14269f4b9e46310d2f3c
Dennis vinterfjärd <dennis@vinterfjard.com>
2022-03-09 20:05:24 +0100

    Initial scaffolding

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/Pixxle/terraform-provider-request/internal/provider"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
+)
+
+func main() {
+
+	debug := flag.Bool("debug", false, "launch provider in debug mode")
+
+	opts := &plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return provider.New()
+		},
+	}
+
+	if *debug {
+		err := plugin.Debug(context.Background(), "github.com/pixxle/terraform-provider-request", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
+}
